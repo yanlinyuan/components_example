b141083389557433e06f4bfbb051bdbe41f9d1a0
yan linyuan <3535058648@qq.com>
2025-01-24 20:47:05 +0800

    Update aggrid.py

--- pages/aggrid.py
+++ pages/aggrid.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
-from streamlit_aggrid import st_aggrid
-from st_aggrid import AgGrid
+from streamlit_aggrid import AgGrid
 
 st.title("Streamlit AgGrid Example: Penguins")
 penguins_df = pd.read_csv("penguins.csv")
