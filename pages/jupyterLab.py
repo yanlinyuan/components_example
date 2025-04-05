1efc421774d0a93869be8894bc57d5f62e389547
yanlinyuan <3535058648@qq.com>
2025-04-05 13:06:09 +0800

    components_example

--- pages/jupyterLab.py
+++
@@ -1,9 +0,0 @@
-# 在Streamlit中嵌入Jupyter
-from pyodide.http import open_url
-from IPython.display import display
-
-def show_jupyter():
-    display(IFrame(src="http://172.26.0.246:8870/lab", width="100%", height=800))
-
-st.write("嵌入式Jupyter环境：")
-show_jupyter()
